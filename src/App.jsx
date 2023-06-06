f411d5359dd08f9f5396c3511a06f5a38a26486b
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2023-06-06 16:22:45 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { MainContainer } from './MainContainer.jsx';
+import { Navbar } from './Navbar.jsx';
+import './styles.css';
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <MainContainer />
+    </>
+  );
+}
---
+++ src/Navbar.jsx
@@ -0,0 +1,9 @@
+export function Navbar() {
+  return (
+    <nav>
+      <img className="logo" src="./src/react-logo.png" alt="React Logo" />
+      <h3>React Facts</h3>
+      <h4>React course - Project 1</h4>
+    </nav>
+  );
+}
