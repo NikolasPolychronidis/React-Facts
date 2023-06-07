3f195823a8fe1cd42dd2ddf1d0db6579625194a9
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2023-06-07 15:32:13 +0200

    Troubleshoot image path

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,9 @@
+import logo from './react-logo.png';
+
 export function Navbar() {
   return (
     <nav>
-      <img className="logo" src="./src/react-logo.png" alt="React Logo" />
+      <img className="logo" src={logo} alt="React Logo" />
       <h3>React Facts</h3>
       <h4>React course - Project 1</h4>
     </nav>
